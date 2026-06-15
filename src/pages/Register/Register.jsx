57a2f02e86aba84e145f5cbab3bd6501de1f5019
jacksoncroque <jackson.crq@gmail.com>
2026-06-15 10:51:37 -0300

    Criado a função de registro e já funcionando

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,108 @@
 import { NavLink } from 'react-router';
 
+import { useState } from 'react';
+
+import { useAuthContext } from '../../contexts/AuthContext';
+
 import Button from '../../components/Button/Button';
 import Input from '../../components/Input';
 
 import styles from './Register.module.scss';
 
 const Register = () => {
+  const { handleRegister } = useAuthContext();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [message, setMessage] = useState(null);
+
+  const handleChangeName = (e) => {
+    setName(e.target.value.replace(/\s+/g, ' '));
+  };
+
+  const nameValidation = (name) => {
+    if (name === '') {
+      return 'O nome não pode estar vazio';
+    }
+
+    const regex = /^[A-Za-zÀ-ÖØ-öø-ÿ\s'-]+$/;
+
+    return regex.test(name) ? null : 'Informe um nome válido';
+  };
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const emailValidation = (email) => {
+    if (email === '') {
+      return 'O campo email não pode estar vazio.';
+    }
+
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,})+$/;
+
+    return regex.test(email) ? null : 'Informe um email  válido.';
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const passwordValidation = (password) => {
+    if (password === '') {
+      return 'O campo senha não pode estar vazio';
+    }
+
+    if (password.length < 6) {
+      return 'O campo senha deve conter ao menos 6 caracteres';
+    }
+
+    return null;
+  };
+
+  const handleChangeConfirmPassword = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const confirmPasswordValidation = (confirmPassword) => {
+    if (confirmPassword === '') {
+      return 'Confirme novamente sua senha';
+    }
+
+    if (confirmPassword !== password) {
+      return 'As senhas devem ser iguais em ambos os campos';
+    }
+
+    return null;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const normalizedName = name.trim().replace(/\s+/g, ' ');
+    const normalizedEmail = email.trim().toLowerCase();
+
+    const nameError = nameValidation(normalizedName);
+    const emailError = emailValidation(normalizedEmail);
+    const passwordError = passwordValidation(password);
+    const confirmPasswordError = confirmPasswordValidation(confirmPassword);
+
+    if (nameError) return setMessage(nameError);
+    if (emailError) return setMessage(emailError);
+    if (passwordError) return setMessage(passwordError);
+    if (confirmPasswordError) return setMessage(confirmPasswordError);
+
+    setMessage(null);
+
+    const response = await handleRegister(normalizedName, normalizedEmail, password);
+
+    if (!response.success) {
+      return setMessage(response.error || 'Erro ao criar conta.');
+    }
+  };
+
   return (
     <div>
       <div className={styles.container}>
@@ -18,7 +115,8 @@ const Register = () => {
             <h1>Criar sua conta</h1>
             <p>Leva menos de um minuto. Sem foto, sem complicação.</p>
           </div>
-          <form>
+          {message ? <div className={styles.containerWrapperError}>{message}</div> : <></>}
+          <form onSubmit={handleSubmit}>
             <Input
               label="Nome completo"
               text="text"
@@ -26,16 +124,18 @@ const Register = () => {
               name="text"
               id="text"
               placeholder="Como devemos te chamar?"
-              required
+              onChange={handleChangeName}
+              value={name}
             />
             <Input
               label="E-mail"
               text="E-mail"
-              type="email"
+              type="text"
               name="email"
               id="email"
               placeholder="voce@exemplo.com"
-              required
+              onChange={handleChangeEmail}
+              value={email}
             />
             <div className={styles.containerWrapperForgotPass}>
               <Input
@@ -44,20 +144,22 @@ const Register = () => {
                 name="password"
                 id="password"
                 placeholder="Mínimo 6 caracteres"
-                required
+                onChange={handleChangePassword}
+                value={password}
               />
             </div>
             <Input
               label="Confirmar senha"
               type="password"
               name="password"
-              id="password"
+              id="confirmPassword"
               placeholder="Repita a senha"
-              required
+              onChange={handleChangeConfirmPassword}
+              value={confirmPassword}
             />
             <Button
               type="submit"
-              label={'Entrar'}
+              label={'Criar conta'}
             />
           </form>
           <div className={styles.containerWrapperRegister}>
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -6,7 +6,11 @@ const login = async (email, password) => {
   return res;
 };
 
-const register = (name, email, password) => {};
+const register = async (name, email, password) => {
+  const res = await api.post('auth/register', { name, email, password });
+
+  return res;
+};
 
 const getMe = (token) => {};
 
